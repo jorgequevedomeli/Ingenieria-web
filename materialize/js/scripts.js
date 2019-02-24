8d3defb7fa1acb5138c5794d92278744df144c0d
Jorge Quevedo <Jorge>
2019-02-23 22:02:46 -0300

    primer commit

---
+++ materialize/js/scripts.js
@@ -0,0 +1,8 @@
+function moveFunction (this) {
+	//var instance = M.Dropdown.getInstance(elem);
+	this.open();
+}
+function outFunction (this) {
+	//var instance = M.Dropdown.getInstance(elem);
+	this.close();
+}
