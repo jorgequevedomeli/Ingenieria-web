04622b0d7683738ba803a05d8fb869ff8962575f
Jorge Quevedo <Jorge>
2019-03-18 19:43:20 -0300

    asdasd

--- conexion.php
+++ conexion.php
@@ -10,10 +10,9 @@
     ini_set('display_errors', 1);
     $conn_string = "host=ec2-54-225-95-183.compute-1.amazonaws.com port=5432 dbname=d5opu7ou0j7j9p user=arnsbnwzexbcsz password=27e8d3c5a51945ea73c500968107e4b61aaddb8edba3cd8107640122a1bc870e";
     $dbconn4 = pg_connect($conn_string) or die("ERROR!!!!!");
-    pg_exec($dbconn4,"CREATE TABLE prueba(edad INT)");
-    pg_query($dbconn4,"insert into prueba values (15)");
-    $re = pg_query($dbconn4,"select * from prueba");
+    //pg_exec($dbconn4,"CREATE TABLE prueba(edad INT)");
+    pg_query($dbconn4,"INSERT INTO prueba VALUES (17)");
+    $re = pg_query($dbconn4,"SELECT * FROM prueba");
     print_r($re);
-    echo $re[0];
-    echo "asdd llega";
+    //echo $re[0];
 ?>
