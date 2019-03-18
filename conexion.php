6e64c8fb421c3ef10561cae1ea842edc6611f755
Jorge Quevedo <Jorge>
2019-03-18 19:30:56 -0300

    prueba exec

--- conexion.php
+++ conexion.php
@@ -8,7 +8,7 @@
     */
     $conn_string = "host=ec2-54-225-95-183.compute-1.amazonaws.com port=5432 dbname=d5opu7ou0j7j9p user=arnsbnwzexbcsz password=27e8d3c5a51945ea73c500968107e4b61aaddb8edba3cd8107640122a1bc870e";
     $dbconn4 = pg_connect($conn_string) or die("ERROR!!!!!");
-    pg_query($dbconn4,"create table prueba(int dato)");
+    pg_exec($dbconn4,"create table prueba(int dato)");
     pg_query($dbconn4,"insert into prueba values (15)");
     $re = pg_query($dbconn4,"select * from prueba");
     print_r($re);
